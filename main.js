ee2cdab721260e3244b2faf271069aa2f14f4b84
anamariareis <142536903+anamariareis@users.noreply.github.com>
2024-09-25 08:31:29 -0300

    4

--- main.js
+++ main.js
@@ -7,3 +7,10 @@ function areaDoquadrado (lado){
     return lado**2
 }
 console.log(areaDoquadrado(6))
+
+let media= function(numero1,numero2,numero3){
+    return numero1+numero2+numero3/3
+}
+console.log(media(1,2,3) )
+let nome=prompt('nome')
+documentwrite(nome)
